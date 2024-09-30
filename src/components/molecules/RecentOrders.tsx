b89c266e1a2c789a699b29177cba840c7d20c408
takaki <takaki55730317@gmail.com>
2024-09-30 18:47:12 +0900

    ダッシュボードと最近の注文画面のレスポンシブ対応を実装
    - RecentOrders.tsxで、モバイルデバイス向けにテーブルのサイズとレイアウトを調整
    - useBreakpointValueを使用して、テーブルのカラム幅をモバイルデバイスに合わせて自動調整
    - DashboardTemplate.tsxにて、カラム数をデバイスの画面サイズに応じて変更
    - Skeletonコンポーネントの表示レイアウトを改善し、モバイル版でのユーザビリティを向上

--- src/components/molecules/RecentOrders.tsx
+++ src/components/molecules/RecentOrders.tsx
@@ -9,12 +9,14 @@ import {
   Th,
   Td,
   Badge,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { TableSkeleton } from "../atoms/SkeletonComponents";
 import { useLoading } from "../../hooks/useLoading";
 
 const RecentOrders: React.FC = () => {
   const isLoading = useLoading();
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   if (isLoading) {
     return <TableSkeleton />;
@@ -25,34 +27,36 @@ const RecentOrders: React.FC = () => {
       <Heading as="h3" size="md" mb={4}>
         最近の注文
       </Heading>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>注文ID</Th>
-            <Th>顧客名</Th>
-            <Th>金額</Th>
-            <Th>状態</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>#1234</Td>
-            <Td>山田太郎</Td>
-            <Td>¥12,000</Td>
-            <Td>
-              <Badge colorScheme="green">配送中</Badge>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>#1235</Td>
-            <Td>佐藤花子</Td>
-            <Td>¥80,500</Td>
-            <Td>
-              <Badge colorScheme="yellow">準備中</Badge>
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
+      <Box overflowX={isMobile ? "auto" : "visible"}>
+        <Table variant="simple" size={isMobile ? "sm" : "md"}>
+          <Thead>
+            <Tr>
+              <Th whiteSpace="nowrap" minWidth="80px">注文ID</Th>
+              <Th whiteSpace="nowrap" minWidth="100px">顧客名</Th>
+              <Th whiteSpace="nowrap" minWidth="80px">金額</Th>
+              <Th whiteSpace="nowrap" minWidth="80px">状態</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td whiteSpace="nowrap">#1234</Td>
+              <Td whiteSpace="nowrap">山田太郎</Td>
+              <Td whiteSpace="nowrap">¥12,000</Td>
+              <Td>
+                <Badge colorScheme="green">配送中</Badge>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td whiteSpace="nowrap">#1235</Td>
+              <Td whiteSpace="nowrap">佐藤花子</Td>
+              <Td whiteSpace="nowrap">¥80,500</Td>
+              <Td>
+                <Badge colorScheme="yellow">準備中</Badge>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Box>
     </Box>
   );
 };
